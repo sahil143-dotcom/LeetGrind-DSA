91ed6f2a00e60a122cb4ba7bc20423ce7ad8da5a
Shaik Sahil Ahammad <sahilahamedshaik58@gmail.com>
2026-08-09 08:40:52 +0530

    Time: 22 ms (31.00%) | Memory: 25 MB (80.59%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> ans;
+
+        for (string s : strs) {
+            string k = s;                       // copy original
+            sort(k.begin(), k.end());           // sort the copy → fingerprint
+            ans[k].push_back(s);                // group original under fingerprint
+        }
+
+        vector<vector<string>> result;
+        for (auto& pair : ans) {
+            result.push_back(pair.second);      // extract all groups
+        }
+        return result;
+    }
+};
