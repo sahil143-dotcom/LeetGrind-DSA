46ea06c835516a3adede7d6c6be0af4a21321e54
Shaik Sahil Ahammad <sahilahamedshaik58@gmail.com>
2026-08-09 08:34:42 +0530

    Time: 4 ms (23.05%) | Memory: 9.4 MB (30.76%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char>map1;
+        unordered_map<char, char>map2;
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            char ch1 = s[i];
+            char ch2 = t[i];
+            if(map1.find(ch1)==map1.end()&&map2.find(ch2)==map2.end()){
+                map1[ch1]=ch2;
+                map2[ch2]=ch1;
+            }
+            else if(map1[ch1]!=ch2||map2[ch2]!=ch1) return false;
+        }
+        return true;
+
+        
+    }
+};
