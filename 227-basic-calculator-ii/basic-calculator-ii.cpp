266aec777c6f38e768ac95a98f978b6ea83c6b1a
Shaik Sahil Ahammad <sahilahamedshaik58@gmail.com>
2026-08-16 18:18:50 +0530

    Time: 7 ms (63.87%) | Memory: 15.3 MB (27.97%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.cpp
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -1,42 +1,39 @@
-//If this solution helped you consider giving it an upvote
 class Solution {
 public:
     int calculate(string s) {
         stack<int> st;
-        int n = 0;
-        char op = '+';
-
-        s += '+';
-
-        for(char c : s){
-            if(c == ' ') continue;
-
-            if(isdigit(c)){
-                n = n * 10 + (c - '0');
-                continue;
+        int num =0;
+        char operation = '+';
+        for(int i = 0; i < s.length(); i++){
+            if(isdigit(s[i])){
+                num = num * 10 + (s[i] - '0');
             }
-
-            if(op == '+') st.push(n);
-            else if(op == '-') st.push(-n);
-            else if(op == '*'){
-                int top = st.top(); st.pop();
-                st.push(top * n);
+            if((!isdigit(s[i]) && s[i] != ' ') || i == s.length() - 1){
+                if(operation == '+'){
+                    st.push(num);
+                }
+                else if (operation == '-'){
+                    st.push(-num);
+                }
+                else if (operation == '*'){
+                    int top = st.top();
+                    st.pop();
+                    st.push(top * num);
+                }
+                else if (operation == '/'){
+                    int top = st.top();
+                    st.pop();
+                    st.push(top / num);
+                }
+                operation = s[i];
+                num = 0;
             }
-            else if(op == '/'){
-                int top = st.top(); st.pop();
-                st.push(top / n);
-            }
-
-            op = c;
-            n = 0;
         }
-
-        int res = 0;
-        while(!st.empty()){
-            res += st.top();
+        int answer = 0;
+        while (!st.empty()){
+            answer += st.top();
             st.pop();
         }
-
-        return res;
+        return answer;
     }
 };
