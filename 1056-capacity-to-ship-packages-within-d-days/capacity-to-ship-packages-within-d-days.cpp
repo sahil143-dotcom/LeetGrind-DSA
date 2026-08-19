be4622e1e7597786068dcf6aa9d758c85c3453ce
Shaik Sahil Ahammad <sahilahamedshaik58@gmail.com>
2026-08-19 06:42:08 +0530

    Time: 16 ms (19.44%) | Memory: 35 MB (54.51%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
-
-    bool isPossible(vector<int>& weights, int days, int mid) {
-
-        int countDays = 1;
-        int sum = 0;
-
-        for(int i = 0; i < weights.size(); i++) {
-
-            if(sum + weights[i] <= mid) {
-                sum += weights[i];
-            }
-            else {
-                countDays++;
-                sum = weights[i];
-            }
-        }
-
-        return countDays <= days;
-    }
-
     int shipWithinDays(vector<int>& weights, int days) {
-
-        int low = *max_element(weights.begin(), weights.end());
-        int high = accumulate(weights.begin(), weights.end(), 0);
-
-        int ans = -1;
-
-        while(low <= high) {
-
-            int mid = low + (high - low) / 2;
-
-            if(isPossible(weights, days, mid)) {
-                ans = mid;
-                high = mid - 1;
-            }
-            else {
-                low = mid + 1;
-            }
+        
+        int totalSum = 0;
+        int maxWeight = INT_MIN;
+        for(int i=0; i<weights.size(); i++){
+            totalSum += weights[i]; 
+            maxWeight = max(maxWeight, weights[i]);
         }
 
+        int s = maxWeight, e = totalSum;
+        int mid = s + (e-s)/2;
+        int ans = e;
+
+        while (s <= e){
+
+            int totalWeight = 0;
+            int count = 1;
+            for(int i=0; i<weights.size(); i++){
+                if(totalWeight + weights[i] > mid){
+                    count++;
+                    totalWeight = 0;
+                }
+                totalWeight += weights[i];
+            }
+            if(count <= days){
+                ans = min(ans, mid);
+                e = mid - 1;
+            }
+            else {
+                s = mid + 1;
+            }
+
+            mid = s + (e-s)/2;
+        }
         return ans;
     }
 };
